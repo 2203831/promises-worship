01749f5c7b962645fb04e1bd29c99e81d3e34378
2203831 <2203831@slu.edu.ph>
2026-04-07 02:02:22 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import BottomNav from "./BottomNav";
 import TopBar from "./TopBar";
+import BottomNav from "./BottomNav";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-dvh">
+    <div className="min-h-screen bg-worship-bg text-worship-text flex flex-col">
+      
+      {/* Top Navigation */}
       <TopBar />
-      <main className="flex-1 pb-24 pt-16 overflow-x-hidden">
+
+      {/* Main Content */}
+      <main className="flex-1 max-w-2xl w-full mx-auto px-4 pt-20 pb-24 animate-fade-in">
         <Outlet />
       </main>
+
+      {/* Bottom Navigation */}
       <BottomNav />
+
     </div>
   );
 }
